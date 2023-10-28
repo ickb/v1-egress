8d1871bf1b5323b8f9fdbe7d3944092d4a84c9bd
phroi <90913182+phroi@users.noreply.github.com>
2023-10-28 14:21:08 +0000

    Show base iCKB functionalities

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { key } from "@ckb-lumos/hd";
 import {
-    TransactionBuilder, defaultScript, getRpc, getSyncedIndexer, initializeChainAdapter, secp256k1SignerFrom
+    epochCompare, getLiveCell, getRpc, getSyncedIndexer, initializeChainAdapter, parseEpoch, secp256k1SignerFrom
 } from "lumos-utils";
 
-import { IckbTransactionBuilder, newLimitOrderUtils, fund, deposit } from "v1-core";
+import { IckbTransactionBuilder, fund, deposit, withdrawFrom } from "v1-core";
 
 import config from "./config.json";
 import { Config } from "@ckb-lumos/config-manager";
-import { BI, parseUnit } from "@ckb-lumos/bi";
-import { Script } from "@ckb-lumos/base";
+import { parseUnit } from "@ckb-lumos/bi";
 import { Account, asyncSleep, randomSecp256k1Account } from "./account";
 
 async function main() {
@@ -51,6 +49,41 @@ async function main() {
         )
     ).buildAndSend();
     console.log("Bot deposit half of the funds with " + depositTx);
+
+    const depositAtIndex0 = await getLiveCell({ txHash: depositTx, index: "0x0" });
+    const { txHash: withdrawalRequest } = await (
+        await fund(
+            withdrawFrom(newTransactionBuilder(botAccount), depositAtIndex0)
+        )
+    ).buildAndSend();
+    console.log("Bot withdraw first deposit " + withdrawalRequest);
+
+    const withdrawalRequestAtIndex0 = await getLiveCell({ txHash: withdrawalRequest, index: "0x0" });
+    while (true) {
+        const b = newTransactionBuilder(botAccount);
+        const tipEpoch = parseEpoch((await getRpc().getTipHeader()).epoch);
+        const maturityEpoch = parseEpoch(await b.withdrawedDaoSince(withdrawalRequestAtIndex0));
+        if (epochCompare(maturityEpoch, tipEpoch) < 1) {//Withdrawal request is ripe
+            break
+        }
+        console.log("Waiting 10 seconds...")
+        await asyncSleep(10000);
+    }
+
+    const { txHash: botSelfSendTx } = await (
+        await fund(
+            newTransactionBuilder(botAccount)
+                .add("output", "end", {
+                    cellOutput: {
+                        capacity: parseUnit("1000", "ckb").toHexString(),
+                        lock: botAccount.lockScript,
+                        type: undefined,
+                    },
+                    data: "0x"
+                })
+        )
+    ).buildAndSend();
+    console.log("Bot botSelfSendTx " + botSelfSendTx);
 }
 
 main();
