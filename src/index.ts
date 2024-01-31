b5bbcc2707f42e78cfac476a9507bce50c92117e
phroi <90913182+phroi@users.noreply.github.com>
2024-01-31 17:53:54 +0000

    Use new version of Lumos Utils and iCKB Core

--- src/index.ts
+++ src/index.ts
@@ -1,149 +1,222 @@
-import {
-    defaultScript,
-    epochCompare, getLiveCell, getRpc, getSyncedIndexer, initializeChainAdapter, parseEpoch, secp256k1SignerFrom
-} from "lumos-utils";
-
-import { IckbTransactionBuilder, fund, deposit, withdrawFrom, newLimitOrderUtils, ICKB_SOFT_CAP_PER_DEPOSIT, ckbSoftCapPerDeposit, ickbEquivalentValue } from "v1-core";
-
 import config from "./config.json";
 import { Config } from "@ckb-lumos/config-manager";
-import { BI, parseUnit } from "@ckb-lumos/bi";
-import { Account, asyncSleep, randomSecp256k1Account } from "./account";
-import { CellCollector } from "@ckb-lumos/ckb-indexer";
-import { Cell, Header } from "@ckb-lumos/base";
+import { parseUnit } from "@ckb-lumos/bi";
+import {
+    Assets,
+    I8Cell, I8Header, I8Script, addCells, capacitiesSifter, ckbFundAdapter, errorNotEnoughFunds,
+    fund, getCells, getHeaderByNumber, getTipHeader, initializeChainAdapter, isDaoDeposit,
+    isDaoWithdrawalRequest, secp256k1Blake160, sendTransaction
+} from "@ickb/lumos-utils";
+import {
+    ICKB_SOFT_CAP_PER_DEPOSIT, ickbDeposit, ickbLogicScript, ickbRequestWithdrawalWith, ickbSifter,
+    ickbSudtFundAdapter, limitOrder
+} from "@ickb/v1-core";
+import { TransactionSkeleton, TransactionSkeletonType } from "@ckb-lumos/helpers";
+import { Cell, Hexadecimal, OutPoint } from "@ckb-lumos/base";
+
+//ADD some check to check initial bot capital
 
 async function main() {
     await initializeChainAdapter("devnet", config as Config);
-    const { fulfill } = newLimitOrderUtils();
+    const limitOrderInfo = limitOrder();
 
     const botAccount = await createBot();
 
-    const { txHash } = await (await rebalance(newTransactionBuilder(botAccount))).buildAndSend();
-
-    console.log(txHash);
-
-    console.log("Start matching orders")
     while (true) {
-        await asyncSleep(1000);
+        console.log("Zzzz...")
+        await new Promise(r => setTimeout(r, 10000));
 
-        let tb = newTransactionBuilder(botAccount);
+        const cells = [
+            ...await getCells({
+                script: botAccount.lockScript,
+                scriptType: "lock",
+                scriptSearchMode: "exact"
+            }),
+            ...await getCells({
+                script: ickbLogicScript(),
+                scriptType: "lock",
+                scriptSearchMode: "exact"
+            }),
+        ];
 
-        //Assume the bot has infinitely big capital
-        const unlimitedBalance = BI.from(10000000000000000000000000000000000000);
-        for (const limitOrder of await getLimitOrders()) {
-            const matched = fulfill(limitOrder, unlimitedBalance, unlimitedBalance)
-            tb = tb.add("input", "start", limitOrder).add("output", "start", matched);
-            console.log("Match limit order");
-        }
+        const { owned: capacities, unknowns } = capacitiesSifter(cells, botAccount.expander);
+        const { ickbGroups, sudts, ickbDeposits } = await myIckbSifter(unknowns, botAccount.expander);
 
-        //Re-balance holding between CKB and iCKB
-        tb = await rebalance(tb);
+        const tipHeader = await getTipHeader();
+        // const feeRate = await getFeeRate();
+        const feeRate = 1000;
 
-        if (tb.outputs.length > 0) {
-            const { txHash } = await tb.buildAndSend();
-            console.log(txHash);
-        }
-    }
-}
+        let assets = ckbFundAdapter(botAccount.lockScript, feeRate, botAccount.preSigner, capacities);
+        assets = ickbSudtFundAdapter(assets, botAccount.lockScript, sudts, tipHeader, ickbGroups);
 
-async function getDeposits() {
-    const indexer = await getSyncedIndexer();
-    //Collect all deposits
-    const res: Cell[] = [];
-    for await (const deposit of new CellCollector(indexer, {
-        scriptSearchMode: "exact",
-        withData: true,
-        type: defaultScript("DAO"),
-        lock: defaultScript("DOMAIN_LOGIC")
-    }).collect()) {
-        res.push(deposit);
-    }
+        const limitOrders = limitOrderInfo.sifter(await getCells({
+            script: limitOrderInfo.limitOrderLock,
+            scriptType: "lock",
+            scriptSearchMode: "prefix"
+        })).owned;
 
-    return res;
-}
+        let tx = TransactionSkeleton();
+        let fundedTx = rebalanceAndFund(tx, assets, ickbDeposits, tipHeader);
 
-async function getLimitOrders() {
-    const indexer = await getSyncedIndexer();
-    const { extract } = newLimitOrderUtils();
-    //Collect all limit orders
-    const res: Cell[] = [];
-    for await (const limitOrder of new CellCollector(indexer, {
-        scriptSearchMode: "prefix",
-        withData: true,
-        lock: defaultScript("LIMIT_ORDER")
-    }).collect()) {
-        try {//Validate limit order
-            extract(limitOrder);
-            res.push(limitOrder);
-        } catch {
-            //Ignore limit order cell
-        }
-    }
-    return res;
-}
-
-async function rebalance(tb: IckbTransactionBuilder) {
-    const tipHeader = await getRpc().getTipHeader();
-    tb = await fund(tb, true, tipHeader);
-    const ickbDelta = await tb.getIckbDelta();
-    const ckbDelta = await tb.getCkbDelta();
-    const ickbEquivalentDelta = ickbEquivalentValue(ckbDelta, tipHeader);
-
-    if (ickbDelta.lt(ickbEquivalentDelta)) {
-        const ckbExcess = ickbEquivalentDelta.sub(ickbDelta).mul(ckbDelta).div(2).div(ickbEquivalentDelta);
-        const depositAmount = ckbSoftCapPerDeposit(tipHeader);
-        const depositQuantity = ckbExcess.div(depositAmount).toNumber();
-        tb = deposit(tb, depositQuantity, depositAmount);
-        for (let i = 0; i < depositQuantity; i++) {
-            console.log("Deposit");
-        }
-    } else {
-        for (const deposit of await getDeposits()) {
-            const ickbDelta = await tb.getIckbDelta();
-            const ckbDelta = await tb.getCkbDelta();
-            const ickbEquivalentDelta = ickbEquivalentValue(ckbDelta, tipHeader);
-            if (ickbDelta.lt(ickbEquivalentDelta)) {
-                break;
+        for (const limitOrder of limitOrders) {
+            tx = limitOrderInfo.fulfill(tx, limitOrder, undefined, undefined);
+            //Re-balance holding between CKB and iCKB
+            let newFundedTx = rebalanceAndFund(tx, assets, ickbDeposits, tipHeader);
+            if (!newFundedTx) {
+                break
             }
-            tb = withdrawFrom(tb, deposit);
-            console.log("Withdraw");
+            fundedTx = newFundedTx;
+        }
+        if (!fundedTx) {
+            continue
+        }
+
+        // console.log(JSON.stringify(fundedTx, undefined, 2));
+        console.log("CKB :", assets["CKB"].balance.div(100000000).toString());
+        console.log("ICKB:", assets["ICKB_SUDT"].balance.div(100000000).toString());
+        fundedTx.inputs.filter(limitOrderInfo.isValid).forEach(() => console.log("Limit Order Matched"));
+        fundedTx.outputs.filter(isDaoDeposit).forEach(() => console.log("Deposit"));
+        fundedTx.outputs.filter(isDaoWithdrawalRequest).forEach(() => console.log("Withdrawal Request"));
+        fundedTx.inputs.filter(isDaoWithdrawalRequest).forEach(() => console.log("Withdrawal"));
+
+        const txHash = await sendTransaction(botAccount.signer(fundedTx));
+        console.log(txHash);
+    }
+}
+
+function rebalanceAndFund(
+    tx: TransactionSkeletonType,
+    assets: Assets,
+    ickbDepositPool: readonly I8Cell[],
+    tipHeader: I8Header
+) {
+    //Balance should be after the current transaction, so it should account for transaction cells
+    const a = Object.freeze(//after Tx Balances
+        Object.fromEntries(
+            Object.entries(assets).map(([name, { balance, availableBalance, getDelta }]) => {
+                const delta = getDelta(tx);
+                return [name, Object.freeze({
+                    balance: balance.add(delta),
+                    availableBalance: availableBalance.add(delta)
+                })];
+            })
+        )
+    );
+
+    for (const [name, { availableBalance }] of Object.entries(a)) {
+        if (availableBalance.lt(0)) {
+            return undefined;
         }
     }
 
-    return tb;
+    //Ideally keep a SUDT balance between one and three deposits, with two deposits being the perfect spot
+    const ickbBalance = a["ICKB_SUDT"].balance;
+    if (ickbBalance.lt(ICKB_SOFT_CAP_PER_DEPOSIT)) {
+        //One deposit for each transaction
+        tx = ickbDeposit(tx, 1, tipHeader);
+    } else if (ickbBalance.gt(ICKB_SOFT_CAP_PER_DEPOSIT.mul(3))) {
+        const ickbExcess = ickbBalance.sub(ICKB_SOFT_CAP_PER_DEPOSIT.mul(2));
+        //One withdrawal request for each transaction
+        tx = ickbRequestWithdrawalWith(tx, ickbDepositPool, tipHeader, ickbExcess, 1);
+    }
+
+    if (tx.outputs.size === 0) {
+        return undefined;
+    }
+
+    try {
+        return fund(tx, assets, true);
+    } catch (e: any) {
+        if (e && e.message === errorNotEnoughFunds) {
+            return undefined;
+        }
+        throw e;
+    }
+
+}
+
+const headerPlaceholder = I8Header.from({
+    compactTarget: "0x1a08a97e",
+    parentHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+    transactionsRoot: "0x31bf3fdf4bc16d6ea195dbae808e2b9a8eca6941d589f6959b1d070d51ac28f7",
+    proposalsHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+    extraHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+    dao: "0x8874337e541ea12e0000c16ff286230029bfa3320800000000710b00c0fefe06",
+    epoch: "0x0",
+    hash: "0x92b197aa1fba0f63633922c61c92375c9c074a93e85963554f5499fe1450d0e5",
+    nonce: "0x0",
+    number: "0x0",
+    timestamp: "0x16e70e6985c",
+    version: "0x0"
+});
+let _blockNum2Header = new Map<string, I8Header>();
+async function myIckbSifter(inputs: readonly Cell[], accountLockExpander: (c: Cell) => I8Script | undefined) {
+    while (true) {//Dangerous/////////////////////////////////////////////////////////////////////////////////
+        const queries: { blockNum: Hexadecimal, context: OutPoint }[] = [];
+        let missingHeaders = false;
+        function getHeader(blockNumber: string, context: Cell): I8Header {
+            queries.push({ blockNum: blockNumber, context: context.outPoint! });
+            let h = _blockNum2Header.get(blockNumber);
+            if (!h) {
+                h = headerPlaceholder;
+                missingHeaders = true;
+            }
+            return h;
+        }
+
+        const result = ickbSifter(inputs, accountLockExpander, getHeader);
+        if (!missingHeaders) {
+            return result;
+        }
+
+        //Discard result, just fetch wanted headers
+        const headers = await getHeaderByNumber(queries, Array.from(_blockNum2Header.values()));
+        _blockNum2Header = new Map(headers.map(h => [h.number, h]));
+    }
 }
 
 async function createBot() {
     console.log("Funding bot");
-    //Genesis account
-    const genesisAccount = randomSecp256k1Account("0xd00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc");
+    //Genesis devnet account
+    const {
+        lockScript,
+        expander,
+        preSigner,
+        signer
+    } = secp256k1Blake160(
+        "0xd00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc"
+    );
+
+    const { owned: capacities } = capacitiesSifter(
+        (await getCells({
+            script: lockScript,
+            scriptType: "lock",
+            filter: {
+                scriptLenRange: ["0x0", "0x1"],
+                outputDataLenRange: ["0x0", "0x1"],
+            },
+            scriptSearchMode: "exact"
+        })),
+        expander
+    );
 
     //Bot account
-    const botAccount = randomSecp256k1Account();
+    const botAccount = secp256k1Blake160();
+    const cell = I8Cell.from({
+        capacity: parseUnit("10000000", "ckb").toHexString(),// == max 50 deposits
+        lock: botAccount.lockScript,
+    });
 
-    const { txHash } = await (
-        await fund(
-            newTransactionBuilder(genesisAccount)
-                .add("output", "end", {
-                    cellOutput: {
-                        capacity: parseUnit("10000000", "ckb").toHexString(),// == max 50 deposits
-                        lock: botAccount.lockScript,
-                        type: undefined,
-                    },
-                    data: "0x"
-                })
-        )
-    ).buildAndSend();
+    let tx = TransactionSkeleton();
+    tx = addCells(tx, "append", [], [cell]);
+    // const feeRate = await getFeeRate();
+    const feeRate = 1000;
+    tx = fund(tx, ckbFundAdapter(lockScript, feeRate, preSigner, capacities));
+    const txHash = await sendTransaction(signer(tx));
+
     console.log(txHash);
 
     return botAccount;
-}
-
-function newTransactionBuilder(account: Account) {
-    return new IckbTransactionBuilder(
-        account.lockScript,
-        secp256k1SignerFrom(account.privKey)
-    );
 }
 
 main();
